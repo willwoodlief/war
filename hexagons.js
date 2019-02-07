50dca4814648613bd9050bba94e5a7dd75382293
willwoodlief <willwoodlief@gmail.com>
2019-02-07 10:41:25 -0600

    parts array is in own class with methods, also will look for skipping while dragging

--- hexagons.js
+++ hexagons.js
@@ -33,10 +33,15 @@
         // noinspection JSUnusedGlobalSymbols
         this.id = id;
         this.color = color;
-        // noinspection JSUnusedGlobalSymbols
-        this.name = name;
+        /**
+         * @var {string} name
+         */
+        this.player_name = name;
     }
 
+    /**
+     * @type {PlayerInfoStruct[]} player_info
+     */
     let player_info= [];
     for(let i = 0; i < NUMBER_PLAYERS; i++) {
         let color = player_colors[i];
@@ -45,6 +50,14 @@
     }
 
 
+    /**
+     *
+     * @param {Number} player_id
+     * @param {Number} start_tick
+     * @param {Number} att
+     * @param {Number} def
+     * @constructor
+     */
     function Part(player_id,start_tick,att,def) {
         this.player_id = player_id;
         this.att = att || 0;
@@ -74,24 +87,220 @@
             return player_info[this.player_id].color;
         };
 
+        this.get_player_name = function() {
+            return player_info[this.player_id].player_name;
+        };
+
         this.get_player_id = function() {
             return this.player_id;
         };
 
-
-
-
-    }
-
-
-    var parts = [];
-    var drags = [];
-    for(var x = 0; x < boardWidth; x++){
-        parts[x] = [];
-        for(var y = 0; y < boardHeight; y++){
-            parts[x][y] = null;
+        // noinspection JSUnusedGlobalSymbols
+        this.toString = function() {
+            let name = this.get_player_name();
+            return name + ": att: " + this.att + " , def: " + this.def;
         }
     }
+
+
+
+    /**
+     *
+     * @param {Number} boardWidth
+     * @param {Number} boardHeight
+     * @constructor
+     */
+    function PartsStuff(boardWidth,boardHeight) {
+
+        this.NE = 0;
+        this.E = 1;
+        this.SE = 2;
+        this.SW = 3;
+        this.W = 4;
+        this.NW = 5;
+        /**
+         * @type {Part[][]} this.parts
+         */
+        this.parts = [];
+        for (let x = 0; x < boardWidth; x++) {
+            this.parts[x] = [];
+            for (let y = 0; y < boardHeight; y++) {
+                this.parts[x][y] = null;
+            }
+        }
+
+        /**
+         *
+         * @param {Number}x
+         * @param {Number}y
+         * @return {null|Part}
+         */
+        this.get_part = function (x, y) {
+            x = parseInt(x.toString());
+            y = parseInt(y.toString());
+            if (x >= 0 && x < boardWidth) {
+                if (y >= 0 && y < boardHeight) {
+                    return this.parts[x][y];
+                }
+            }
+            return null;
+        };
+
+        /**
+         *
+         * @param {Number}x
+         * @param {Number}y
+         * @param {null|Part} part
+         */
+        this.set_part = function (x, y, part) {
+            x = parseInt(x.toString());
+            y = parseInt(y.toString());
+            if (x >= 0 && x < boardWidth) {
+                if (y >= 0 && y < boardHeight) {
+                    if (part) {
+                        if (this.parts[x][y]) {
+                            /**
+                             * @type {Part} old
+                             */
+                            let old = this.parts[x][y];
+                            throw new Error('Tried to set a part on a non empty space! [' + x + ',' + y + '] ' + old.toString());
+                        }
+                    }
+                    this.parts[x][y] = part;
+                    return;
+                }
+            }
+            throw new Error('Tried to set a part out of bounds! [' + x + ',' + y + '] ');
+        };
+
+        /**
+         *
+         * @param {Number}x
+         * @param {Number}y
+         * @return {Part[]}
+         */
+        this.get_all_neighbors = function (x, y) {
+            let ret = [];
+            for (let dir = 0; dir < 6; dir++) {
+                let come = this.get_neighbor(x,y,dir);
+                if (come) {
+                    ret.push(come);
+                }
+            }
+            return ret;
+        };
+
+        /**
+         *
+         * @param {Number} x
+         * @param {Number} y
+         * @return {Number[]}
+         */
+        this.get_players_touching_coords = function(x,y) {
+          let  ret = [];
+          let s = this.get_all_neighbors(x,y);
+          let lookup = {};
+          for(let i = 0; i < s.length; i++) {
+              let other_player_id = s[i].get_player_id();
+              lookup[other_player_id] = s[i];
+          }
+
+          for (let id in lookup) {
+              if (!lookup.hasOwnProperty(id)) {continue;}
+              ret.push(parseInt(id));
+          }
+          return ret;
+
+        };
+
+
+        /**
+         *
+         * @param {Number} x
+         * @param  {Number} y
+         * @param  {Number} dir
+         * @return {Part}
+         */
+        this.get_neighbor = function (x, y, dir) {
+            let rel_x = x;
+            let rel_y = y;
+            let bump = y % 2;
+            if (bump) {
+                //x is 1 to the right
+                switch (dir) {
+                    case this.NE: {
+                        rel_x++;
+                        rel_y--;
+                        break;
+                    }
+                    case this.E: {
+                        rel_x++;
+                        break;
+                    }
+                    case this.SE: {
+                        rel_x++;
+                        rel_y++;
+                        break;
+                    }
+                    case this.SW: {
+                        rel_y++;
+                        break;
+                    }
+                    case this.W: {
+                        rel_x--;
+                        break;
+                    }
+                    case this.NW: {
+                        rel_y--;
+                        break;
+                    }
+                    default: {
+                        throw new Error("direction is only 0 through 5 inclusive, passed was " + dir);
+                    }
+                }
+            } else {
+                //x is 1 to the left
+                switch (dir) {
+                    case this.NE: {
+                        rel_y--;
+                        break;
+                    }
+                    case this.E: {
+                        rel_x++;
+                        break;
+                    }
+                    case this.SE: {
+                        rel_y++;
+                        break;
+                    }
+                    case this.SW: {
+                        rel_y++;
+                        rel_x--;
+                        break;
+                    }
+                    case this.W: {
+                        rel_x--;
+                        break;
+                    }
+                    case this.NW: {
+                        rel_y--;
+                        rel_x--;
+                        break;
+                    }
+                    default: {
+                        throw new Error("direction is only 0 through 5 inclusive, passed was " + dir);
+                    }
+                }
+            }
+            return this.get_part(rel_x, rel_y);
+
+        };
+    }
+
+
+
+    var drags = [];
+    var parts = new PartsStuff(boardWidth,boardHeight);
 // players array, each like colors but filled with class that has def, att, b_is_connected, base_color,last_move_tick
     //sum att for all player's hexes is 1, sum def for all player's hexes is 1
     // if not connected, then each attack diminishes by decay each tick to 0, if connected then grows by decay each tick to limit
@@ -102,8 +311,10 @@
     // will stay connected drawing a thin line to the source
 
     //natural terrain, can modify att or def for that hex, can change how fast can move, can modify decay(-+ or both for all attributes)
-    parts[0][0] = new Part(1,0,.95,.94);
-    parts[boardWidth-1][boardHeight-1] = new Part(0,0,.95,.94);
+    let p  = new Part(1,0,.95,.94);
+    parts.set_part(0,0,p);
+    p = new Part(0,0,.95,.94);
+    parts.set_part(boardWidth-1,boardHeight-1,p);
 
 
 
@@ -141,20 +352,34 @@
                     if (b_mouse_drag) {
 
                         if ( ((n_drag_hex_x !== hexX) || (n_drag_hex_y !== hexY)) && (!drags[hexX][hexY])) {
-                            if (!parts[hexX][hexY]) {
-                                //todo make sure there is at least one similar touching, else the drag skipped a hex
+                            if (!parts.get_part(hexX,hexY) ) {
 
                                 //drag to empty
                                 if (source_drag_part) {
                                     let id = source_drag_part.get_player_id();
-                                    let new_part = new Part(id,current_tick,source_drag_part.att,source_drag_part.def);
-                                    parts[hexX][hexY] = new_part;
+
+                                    // make sure there is at least one similar touching, else the drag skipped a hex
+                                    let player_ids_touching = parts.get_players_touching_coords(hexX,hexY);
+                                    let b_found_same_player = false;
+                                    for(let i= 0; i < player_ids_touching.length; i++) {
+                                        let player_id = player_ids_touching[i];
+                                        if (player_id === id) {
+                                            b_found_same_player = true;
+                                            break;
+                                        }
+                                    }
+                                    if (b_found_same_player) {
+                                        let new_part = new Part(id,current_tick,source_drag_part.att,source_drag_part.def);
+                                        parts.set_part(hexX,hexY,new_part);
+                                    } else {
+                                        valid = false;
+                                    }
                                 }
                             } else {
                                 //if the current is the same player id as the source, then
-                                if (source_drag_part.get_player_id() === parts[hexX][hexY].get_player_id()) {
-                                    parts[n_drag_hex_x][n_drag_hex_y] = null;
-                                    source_drag_part = parts[hexX][hexY];
+                                if (source_drag_part.get_player_id() === parts.get_part(hexX,hexY).get_player_id()) {
+                                    parts.set_part(n_drag_hex_x,n_drag_hex_y,null);
+                                    source_drag_part = parts.get_part(hexX,hexY);
                                 } else {
                                     valid = false;
                                 }
@@ -194,11 +419,7 @@
 
             b_mouse_down = true;
 
-            if (parts[n_drag_hex_x][n_drag_hex_y]) {
-                source_drag_part = parts[n_drag_hex_x][n_drag_hex_y];
-            } else {
-                source_drag_part = null;
-            }
+            source_drag_part = parts.get_part(n_drag_hex_x,n_drag_hex_y);
 
             for(let i = 0; i < boardWidth; i++){
                 drags[i] = [];
@@ -262,8 +483,8 @@
             
         } else {
             //see if part is there
-            if (parts[i][j]) {
-                let part = parts[i][j];
+            if (parts.get_part(i,j)) {
+                let part = parts.get_part(i,j);
                 let color = part.get_color();
                 ctx.fillStyle = color;
                 canvasContext.fill();
